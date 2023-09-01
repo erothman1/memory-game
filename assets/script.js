f75a6339bdf0b766efd9ef5c2db40bb8cc9e2cea
Elva Rothman <erothman@tulane.edu>
2023-08-31 21:38:41 -0400

    wrote evaluteSelections function, but very buggy, needs work

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ const gameContainer = document.getElementById("game-container")
 const controllers = document.getElementById("controllers")
 const stats = document.getElementById("stats")
 const board = document.getElementById("board")
-const cards = document.querySelectorAll(".card")
+// const cards = document.querySelectorAll(".card")
 
 //Create array of items 
 //using the names to help with match evaluation 
@@ -73,12 +73,12 @@ const gameCreation = () => {
 
     const randPicks = pickRandom(dimensions)
 
-    //without the shuffle, only 8 cards will show up because we didn't duplicate them for the matching 
+    //without the shuffle, only 8 cards will show up because we didnt duplicate them for the matching 
     const shufflePicks = shuffle([...randPicks, ...randPicks])
 
     board.innerHTML = ""
 
-    //TODO: when the cards show up, the text isn't centered for some reason
+    //TODO: when the cards show up, the back of the card isnt centered for some reason
     for (let i = 0; i < dimensions * dimensions; i++) {
         board.innerHTML += `
         <div class="card" data-card="${shufflePicks[i].name}">
@@ -90,11 +90,17 @@ const gameCreation = () => {
         `
     }
 
-    //TODO: this event listener isn't working 
+    const cards =  document.querySelectorAll(".card")
+
     cards.forEach(card => {
         card.addEventListener("click", () => {
-            console.log("hi")
-            //TODO: flip cards here
+
+            const cardValue = card.getAttribute("data-card")
+            console.log(cardValue)
+
+            //flip cards here
+            card.classList.toggle("flipped")
+
             evaluateSelections()
         })
     })
@@ -102,14 +108,40 @@ const gameCreation = () => {
 }
 
 //Function to decide correct and wrong answers 
-//TODO: need to figure out how to do this ...
 //use the data-card attribute which uses the name of the emoji to evaluate if matches are correct or not?
 //if correct, keep cards flipped and add a correct class to those cards
 //correct class will add the correct green color to the card 
-//if correct class isn't applied then cards flip back over
+//if correct class isn"t applied then cards flip back over
 const evaluateSelections = () => {
 
-        //TODO: need to add 10 points to score for correct match and subtract 1 point from score for incorrect match 
+    //TODO: this function works until the first match is found; I think maybe because i'm not taking into account that cards are staying flipped?
+    //TODO: score isn't decreasing when answers are wrong, but it does go up when match is correct
+
+    let score = 100
+    
+    const flippedCards = document.querySelectorAll(".card.flipped")
+
+    if (flippedCards.length === 2) {
+        const card1 = flippedCards[0]
+        const card2 = flippedCards[1]
+
+        const card1Value = card1.getAttribute("data-card")
+        const card2Value = card2.getAttribute("data-card")
+
+        if ( card1Value === card2Value ) {
+            card1.classList.add("correct")
+            card2.classList.add("correct")
+            score += 10
+        } else {
+            setTimeout(() => {
+                card1.classList.remove("flipped")
+                card2.classList.remove("flipped")
+                score -= 1
+            }, 1000)
+        }
+
+        stats.textContent = `Score: ${score}`
+    }
 
 }
 
@@ -121,5 +153,6 @@ newGame.addEventListener("click", () => {
 
     gameCreation() 
 
-    //TODO: clicking new game button also needs to reset the score to 100
+    //clicking new game button also needs to reset the score to 100
+    stats.textContent = "Score: 100"
 })
